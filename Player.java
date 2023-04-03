3a9149f3dc5fc406b8ff04a56051d6cf0b5dacd0
Norman5k <48478343+Norman5k@users.noreply.github.com>
2023-04-03 14:35:45 +0700

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,10 @@
+package com.MyApplication;
+public class Player {
+    String nickname;
+    int scores;
+
+    public Player(String nickname, int scores) {
+        this.nickname = nickname;
+        this.scores = scores;
+    }
+}
